78ee68b0ac9743cf51c3975a9c0967828e92c833
Yanick <yanickjmedina@gmail.com>
2026-04-01 23:26:01 +0200

    feat: read env args from cli using std::env. closes #2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
+
 fn main() {
     println!("rtodo - v0.1.0 - your local task manager");
+    let env_args: Vec<String> = env::args().collect();
+
+    if env_args.len() > 1 {
+        println!("Command <{}>", env_args[1]);
+    } else {
+        println!("Usage: rtodo <command>");
+    }
 }
