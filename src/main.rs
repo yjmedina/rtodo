fa96db4008b146bf3b8b20ef344acf7418d8a3e1
Yanick <yanickjmedina@gmail.com>
2026-04-02 11:32:02 +0200

    feat: add if else statement for different commands, closes #3

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,24 @@ fn main() {
     let env_args: Vec<String> = env::args().collect();
 
     if env_args.len() > 1 {
-        println!("Command <{}>", env_args[1]);
+        let command = env_args[1].as_str();
+
+        if command == "new" {
+            println!("[new] create project");
+        } else if command == "ls" {
+            println!("[list] all projects")
+        } else if command == "set" {
+            println!("[set] set active project")
+        } else if command == "delete" {
+            println!("[delete] current project")
+
+        } else if command == "task" {
+            println!("[task] exploring some task")
+        } else {
+            println!("Unknown command: <{command}>")
+        }
+
+
     } else {
         println!("Usage: rtodo <command>");
     }
