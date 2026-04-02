c2eb87cc613714292a56910e6277745ecee4bb47
Yanick <yanickjmedina@gmail.com>
2026-04-02 11:46:59 +0200

    feat: dispatch function, Closes #5

--- src/main.rs
+++ src/main.rs
@@ -21,28 +21,32 @@ fn get_priority_label(priority: u8) -> &'static str {
 
 }
 
+
+fn dispatch(command: &str) {
+    if command == "new" {
+        println!("[new] create project");
+    } else if command == "ls" {
+        println!("[list] all projects")
+    } else if command == "set" {
+        println!("[set] set active project")
+    } else if command == "delete" {
+        println!("[delete] current project")
+
+    } else if command == "task" {
+        println!("[task] exploring some task")
+    } else {
+        println!("Unknown command: <{command}>")
+    }
+
+}
+
+
 fn main() {
     println!("rtodo - v0.1.0 - your local task manager");
     let env_args: Vec<String> = env::args().collect();
 
     if env_args.len() > 1 {
-        let command = env_args[1].as_str();
-
-        if command == "new" {
-            println!("[new] create project");
-        } else if command == "ls" {
-            println!("[list] all projects")
-        } else if command == "set" {
-            println!("[set] set active project")
-        } else if command == "delete" {
-            println!("[delete] current project")
-
-        } else if command == "task" {
-            println!("[task] exploring some task")
-        } else {
-            println!("Unknown command: <{command}>")
-        }
-
+        dispatch(&env_args[1]);
 
     } else {
         println!("Usage: rtodo <command>");
